779049aaf7eabe694988a9652d9eca0cd1a0bd91
Draa7 <janitraendra@gmail.com>
2026-03-12 14:29:14 +0700

    fix: detail sopd pengajuan

--- app/Filament/Resources/SopdPengajuans/Schemas/SopdPengajuanInfolist.php
+++ app/Filament/Resources/SopdPengajuans/Schemas/SopdPengajuanInfolist.php
@@ -2,6 +2,8 @@
 
 namespace App\Filament\Resources\SopdPengajuans\Schemas;
 
+use Filament\Schemas\Components\Section;
+use Filament\Infolists\Components\TextEntry;
 use Filament\Schemas\Schema;
 
 class SopdPengajuanInfolist
@@ -9,8 +11,63 @@ class SopdPengajuanInfolist
     public static function configure(Schema $schema): Schema
     {
         return $schema
+            ->columns(12)
             ->components([
-                //
+                Section::make('Detail Surat')
+                    ->description('Informasi utama surat masuk')
+                    ->columns(12)
+                    ->columnSpanFull()
+                    ->components([
+                        TextEntry::make('tanggal_surat')
+                            ->label('Tanggal Surat')
+                            ->date('d F Y')
+                            ->columnSpan(4),
+
+                        TextEntry::make('no_urut')
+                            ->label('No Urut')
+                            ->badge()
+                            ->columnSpan(2),
+
+                        TextEntry::make('no_surat')
+                            ->label('Nomor Surat')
+                            ->copyable()
+                            ->columnSpan(4),
+
+                        TextEntry::make('kepada')
+                            ->label('Kepada')
+                            ->placeholder('-')
+                            ->columnSpan(6),
+
+                        TextEntry::make('kontak_person')
+                            ->label('Kontak Person')
+                            ->placeholder('-')
+                            ->columnSpan(6),
+
+                        TextEntry::make('perihal')
+                            ->label('Perihal')
+                            ->placeholder('-')
+                            ->prose()
+                            ->columnSpanFull(),
+                    ]),
+
+                Section::make('Data Klasifikasi')
+                    ->description('Informasi klasifikasi dan pengolahan surat')
+                    ->columns(12)
+                    ->columnSpanFull()
+                    ->collapsed()
+                    ->components([
+                        TextEntry::make('unitPengolah.direktorat')
+                            ->label('Direktorat')
+                            ->columnSpan(4),
+
+                        TextEntry::make('Kode.kode')
+                            ->label('Kode')
+                            ->columnSpan(4),
+
+                        TextEntry::make('sifat.sifat_surat')
+                            ->label('Sifat Surat')
+                            ->columnSpan(4),
+                    ]),
             ]);
     }
 }
